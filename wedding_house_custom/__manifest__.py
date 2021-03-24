e3096f9c903fbeb3760c94bb187193d7c5a5d5c9
Rottab <mr.rottab@gmail.com>
2021-03-24 19:26:16 +0200

    first

---
+++ wedding_house_custom/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Wedding House',
+    'description': 'Wedding House Technical Modifications',
+    'version': '0.0.1',
+    'author': 'EffVision',
+    'category': 'POS',
+
+    'depends': ['stock', 'purchase', 'point_of_sale'],
+    'data': [
+
+    ],
+    'application': True,
+}
